a75081e86f286838ab9db053c2fac5a58445d094
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-06-13 21:59:04 +0200

    Functions include file

---
+++ src/functions_include.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WyriHaximus;
+
+// @codeCoverageIgnoreStart
+if (!function_exists('WyriHaximus\get_in_packages_composer')) {
+    require __DIR__ . '/functions.php';
+}
+// @codeCoverageIgnoreEnd
