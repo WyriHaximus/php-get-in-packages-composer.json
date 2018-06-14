fc37b24f0b1c24ffaae91a1fb6903d10c728f8aa
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-06-14 22:31:04 +0200

    Function that just returns the path

--- src/functions.php
+++ src/functions.php
@@ -26,6 +26,23 @@ function from_get_in_packages_composer(string $path, bool $includeRoot = true): 
     }
 }
 
+function get_in_packages_composer_path(string $path, bool $includeRoot = true): iterable
+{
+    /**
+     * @var Package $package
+     * @var mixed $items
+     */
+    foreach (get_in_packages_composer($path, $includeRoot) as $package => $items) {
+        /**
+         * @var mixed $item
+         * @var string $path
+         */
+        foreach ($items as $item => $itemPath) {
+            yield $package->getPath($itemPath);
+        }
+    }
+}
+
 function get_in_packages_composer_with_path(string $path, bool $includeRoot = true): iterable
 {
     /**
