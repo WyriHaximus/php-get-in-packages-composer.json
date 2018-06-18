f392af077b480345eaa086803fc4a4124bdaab18
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-06-18 22:53:51 +0200

    get_in_packages_composer test

---
+++ tests/GetInPackagesComposerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace WyriHaximus\Tests;
+
+use ApiClients\Tools\TestUtilities\TestCase;
+use Composed\Package;
+use function WyriHaximus\get_in_packages_composer;
+
+final class GetInPackagesComposerTest extends TestCase
+{
+    public function testConfig()
+    {
+        $config = [];
+        /**
+         * @var Package $package
+         * @var array $value
+         */
+        foreach (get_in_packages_composer('config') as $package => $value) {
+            $config[$package->getName()] = $value;
+        }
+        self::assertSame(
+            [
+                'wyrihaximus/get-in-packages-composer.jason' => [
+                    'sort-packages' => true,
+                    'platform' => [
+                        'php' => '7.2',
+                    ],
+                ],
+            ],
+            $config
+        );
+    }
+}
