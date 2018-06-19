061fadf76f508ed11c7a9a1522b7c966e004b43a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-06-19 08:11:20 +0200

    get_in_packages_composer_path test

---
+++ tests/GetInPackagesComposerPathTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WyriHaximus\Tests;
+
+use ApiClients\Tools\TestUtilities\TestCase;
+use Composed\Package;
+use function WyriHaximus\get_in_packages_composer_path;
+
+final class GetInPackagesComposerPathTest extends TestCase
+{
+    public function testConfig()
+    {
+        $config = [];
+        /**
+         * @var Package $package
+         * @var array $value
+         */
+        foreach (get_in_packages_composer_path('autoload.files') as $path) {
+            $config[$path] = $path;
+        }
+        self::assertSame(
+            [
+                dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'functions_include.php' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'functions_include.php',
+                dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'igorw' . DIRECTORY_SEPARATOR . 'get-in' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'get_in.php' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'igorw' . DIRECTORY_SEPARATOR . 'get-in' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'get_in.php',
+                dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'joshdifabio' . DIRECTORY_SEPARATOR . 'composed' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'functions_include.php' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'joshdifabio' . DIRECTORY_SEPARATOR . 'composed' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'functions_include.php',
+            ],
+            $config
+        );
+    }
+}
